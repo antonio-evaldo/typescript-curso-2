111d413461e5c75392129c05ba69d06709685cc2
Antônio Evaldo <antonio.evaldo349@gmail.com>
2021-10-11 09:44:28 -0300

    first commit

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,42 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes;
+
+  constructor() {
+    this.inputData = document.querySelector('#data');
+    this.inputQuantidade = document.querySelector('#quantidade');
+    this.inputValor = document.querySelector('#valor');
+  }
+
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+
+    negociacao.data.setDate(12); // não altera a data original
+
+    this.negociacoes.adiciona(negociacao);
+    const negociacoes = this.negociacoes.lista();
+    console.log(negociacoes);
+    this.limparFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
+    const exp = /-/g;
+    const data = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+
+    return new Negociacao(data, quantidade, valor);
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
+  }
+}
